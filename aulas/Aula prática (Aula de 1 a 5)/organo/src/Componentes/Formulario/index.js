3e9bbc6bb8a2e517511799e101dd6259dad201bd
robertdouglasaimon <robertdouglasaimon@gmail.com>
2024-07-05 13:33:52 -0300

    UPDATE

--- aulas/Aula prática (Aula de 1 a 5)/organo/src/Componentes/Formulario/index.js
+++ aulas/Aula prática (Aula de 1 a 5)/organo/src/Componentes/Formulario/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Botao from '../Botao';
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
@@ -16,20 +17,52 @@ const Formulario = () => {
 
     ]
 
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log('Form foi submetido')
+        console.log('Form foi submetido =>', nome, cargo, imagem, time)
     }
 
     return (
         <section className="formulario">
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
-                <CampoTexto obrigatorio={true}  label='Nome' placeholder='Digite seu nome'/>
-                <CampoTexto obrigatorio={true}  label='Cargo' placeholder='Digite seu cargo'/>
-                <CampoTexto label='Imagem' placeholder='Informe o endereço da imagem'/>  
-                <ListaSuspensa  label='Time' itens={times}/> 
-                <Botao>
+                <CampoTexto 
+                    obrigatorio={true}  
+                    label='Nome' 
+                    placeholder='Digite seu nome'
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+
+                <CampoTexto 
+                    obrigatorio={true}  
+                    label='Cargo' 
+                    placeholder='Digite seu cargo'
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}
+                />
+
+                <CampoTexto 
+                    label='Imagem' 
+                    placeholder='Informe o endereço da imagem'
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                /> 
+
+                <ListaSuspensa  
+                    label='Time' 
+                    itens={times}
+                    valor={time}
+                    aoAlterado={valor => setTime(valor)}
+
+                /> 
+
+                <Botao> {/* <------- Aninhamento de componenetes!*/}
                     Criar Card
                 </Botao>
             </form>
