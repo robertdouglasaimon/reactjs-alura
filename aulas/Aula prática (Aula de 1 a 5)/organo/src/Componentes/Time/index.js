936122935b25aaf86701bdcfd360b7e8fe5cd454
robertdouglasaimon <robertdouglasaimon@gmail.com>
2024-07-26 12:11:36 -0300

    UPDATE

--- aulas/Aula prática (Aula de 1 a 5)/organo/src/Componentes/Time/index.js
+++ aulas/Aula prática (Aula de 1 a 5)/organo/src/Componentes/Time/index.js
@@ -1,18 +1,20 @@
+import hexToRgba from 'hex-to-rgba';
 import Colaborador from '../Colaborador'
 import Fundo from '../Rodape/imagens/fundo.png'
 import './time.css'
 
-const Time = ({ time, colaboradores, aoDeletar }) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage:`url(${Fundo})`, 
-        backgroundColor: time.corPrimaria }}>
-            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+        backgroundColor: hexToRgba(time.cor,'0.6')}}>
+            <input onChange={evento => mudarCor(evento.target.value, time.nome)} value={time.cor} type='color' className='input-cor'/>
+            <h3 style={{ borderColor: time.cor}}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
                     return  <Colaborador 
                     key={indice} 
                     colaborador={colaborador} 
-                    corDeFundo={time.corSecundaria} 
+                    corDeFundo={time.cor} 
                     aoDeletar={aoDeletar}/>
                 })}
             </div>
