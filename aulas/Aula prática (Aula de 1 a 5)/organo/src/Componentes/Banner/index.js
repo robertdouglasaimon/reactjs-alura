ab27c03e3f96f0fc731c8f0cf40005437811ef68
robertdouglasaimon <robertdouglasaimon@gmail.com>
2024-08-01 11:16:43 -0300

    Update index.js

--- aulas/Aula prática (Aula de 1 a 5)/organo/src/Componentes/Banner/index.js
+++ aulas/Aula prática (Aula de 1 a 5)/organo/src/Componentes/Banner/index.js
@@ -1,7 +1,7 @@
 import './banner.css';
 import FotoBanner from '../Rodape/imagens/banner.png'
 
-const Banner = () => {
+const Banner = (props) => {
     return (
         <header className="cabecalho">
             <img src={FotoBanner} alt='Logo do Organo'/>
