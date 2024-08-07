459c585d40d2f7a989cf81ec363dbe8c344e0306
robertdouglasaimon <robertdouglasaimon@gmail.com>
2024-08-07 10:33:01 -0300

    UPDATE

---
+++ aulas/aula-pratica(1 a 5)/ola-mundo/src/Componentes/Menu/index.js
@@ -0,0 +1,21 @@
+import { Link, useLocation } from 'react-router-dom';
+import styles from'./Menu.module.css'
+import MenuLink from '../MenuLink';
+
+const Menu = () => {
+
+    return (
+        <header>
+            <nav className={styles.navegacao}>
+                <MenuLink to='/'>
+                    Inicio
+                </MenuLink>
+                <MenuLink to='/sobremim'>
+                    Sobre Mim
+                </MenuLink>
+            </nav>
+        </header>
+    )
+}
+
+export default Menu;
---
+++ aulas/aula-pratica(1 a 5)/ola-mundo/src/Routes.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes,  Route } from "react-router-dom"
+import Inicio from "./Componentes/Inicio";
+import SobreMim from "./Componentes/SobreMim";
+import Menu from "./Componentes/Menu";
+
+
+
+function AppRoutes(props) {
+  return (
+    <BrowserRouter>
+      <Menu/>
+      <Routes>
+        <Route path='/' element={<Inicio/>} />
+        <Route path='/sobremim' element={<SobreMim/>} />
+        <Route path='*' element={<div>Página não encontrada.</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
